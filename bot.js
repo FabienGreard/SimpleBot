1c813c45e5fb29b241b2f6658da35d0d8c835fbf
Fabien <grd.fabien@gmail.com>
2017-08-09 14:23:22 +0200

    fix + add roulette game

--- bot.js
+++ bot.js
@@ -1,10 +1,14 @@
 // Discord.js
-var Discord = require("discord.js");
+let Discord = require("discord.js");
 require("opusscript");
 
 // autoReconnect is enabed
-var bot = new Discord.Client({autoReconnect: true});
-var music = require('discord.js-music-v11');
+let bot = new Discord.Client({autoReconnect: true});
+let music = require('discord.js-music-v11');
+
+/* config russian game*/
+let numberHitleft = 6;
+let game = true;
 
 music(bot);
 // Set the prefix
@@ -14,8 +18,8 @@ let prefix = "/";
 bot.on('ready', () => {
 
   bot.user.setStatus("online"); //dnd , online , ldle
-  bot.user.setGame("Manger des noix");
-  //bot.user.setGame("In Dev");
+  //bot.user.setGame("Manger des noix");
+  bot.user.setGame("In Dev");
 
   console.log("Oui messires ! Encore du travail ?!");
 });
@@ -31,31 +35,58 @@ bot.on("message", msg => {
 
       // Command /help
      if (msg.content.startsWith(prefix + "help")) {
-      msg.channel.send("```List of commands : \n\n /help | list all commands \n /rand | random beetween 1-100 \n !play <url|search> | Play a video/music  \n !skip [number] | Skip some number of songs. Will skip 1 song if a number is not specified. \n !queue | Display the current queue. \n !pause | Pause music playback. \n !resume | Resume music playback \n !leave | Clears the song queue and leaves the channel. \n !clearqueue | Clears the song queue.\n /version  | Version ```");
+      msg.channel.send("```List of commands : \n\n /help | list all commands \n /rand | random beetween 0-100 \n /roulette | russian game \n /version  | Version \n !play <url|search> | Play a video/music  \n !skip [number] | Skip some number of songs. Will skip 1 song if a number is not specified. \n !queue | Display the current queue. \n !pause | Pause music playback. \n !resume | Resume music playback \n !leave | Clears the song queue and leaves the channel. \n !clearqueue | Clears the song queue. ```");
       console.log("Command executed: /help")
     }
     // Command /version
     if (msg.content.startsWith(prefix + "version")) {
-      msg.channel.send("``` Bot Discord Basic - Version 1.0.0 \n Créateur : IMAGOODGUY ```");
+      msg.channel.send("``` Bot Discord Basic - Version 1.1.0 \n Créateur : IMAGOODGUY ```");
       console.log("Command executed : /version")
     }
 
     if (msg.content.startsWith(prefix +"rand")) {
       // Command /rand
       rand = Math.floor(Math.random() * 100);
-      msg.reply("``` " + rand + " ```");
+      msg.reply(" a fait " + rand + " ");
       console.log(rand);
    }
 
-   if(msg.content.startsWith("tchoin") || msg.content.startsWith("pute")){
-     // start with choin or pute
-     let role = message.guild.roles.find("name", "Inis Mom").members;
-
-     //msg.channel.send("``` On parle de TalissaLlil ? ```");
-     msg.channel.send(role);
-
-     console.log("choin or pute");
+   if(msg.content.toLowerCase().indexOf("tchoin") !== -1 || msg.content.toLowerCase().indexOf("pute") !== -1){
+     // contains tchoin or pute
+     //310541740387860491 talisal
+     msg.channel.send(`On a besoin de toi ${bot.users.get("310541740387860491")} !`);
+     console.log(msg.content.toLowerCase());
    }
+
+   if(msg.content.startsWith(prefix + "roulette")){
+     // Comand /roulette
+
+     let hit = Math.floor(Math.random() * numberHitleft) === 0
+
+      console.log(` coup restant : ${numberHitleft}, jeu : ${game}, tir : ${hit}`);
+
+      if(numberHitleft === 6){ // check if start or is playing
+        msg.channel.send("``` Chargement du révolver.. ```");
+      }else{
+        msg.channel.send("``` il reste " + numberHitleft + " coups ```");
+      }
+
+      if(hit){ //shot
+        msg.reply(" est mort... Dommage (: ");
+        game = false;
+      }else{
+        msg.reply(" tu n'est pas mort ! ");
+        numberHitleft--;
+      }
+
+      if(game === false || numberHitleft === 1){ // check if end
+        msg.channel.send("``` La partie est terminé ```");
+        numberHitleft = 6;
+        game = true;
+      }
+
+   }
+
  });
 
 bot.login('MzQ0NDI3MTE2ODUyNDEyNDE3.DGs1EA.mSobi3ZLKdiSIzNmQQ_zv7pw1N8');
