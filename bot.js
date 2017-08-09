484d931c22388e48a3ffa0bff8fca5c56bc79319
Fabien <grd.fabien@gmail.com>
2017-08-09 15:22:41 +0200

    add tictac game

--- bot.js
+++ bot.js
@@ -8,7 +8,12 @@ let music = require('discord.js-music-v11');
 
 /* config russian game*/
 let numberHitleft = 6;
-let game = true;
+let gameRussian = true;
+
+/* config TicTac game*/
+let gameTicTac = true;
+let gameBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let lastPion = "X";
 
 music(bot);
 // Set the prefix
@@ -35,12 +40,12 @@ bot.on("message", msg => {
 
       // Command /help
      if (msg.content.startsWith(prefix + "help")) {
-      msg.channel.send("```List of commands : \n\n /help | list all commands \n /rand | random beetween 0-100 \n /roulette | russian game \n /version  | Version \n !play <url|search> | Play a video/music  \n !skip [number] | Skip some number of songs. Will skip 1 song if a number is not specified. \n !queue | Display the current queue. \n !pause | Pause music playback. \n !resume | Resume music playback \n !leave | Clears the song queue and leaves the channel. \n !clearqueue | Clears the song queue. ```");
+      msg.channel.send("```List of commands : \n\n /help | list all commands \n /rand | random beetween 0-100 \n /roulette | russian game \n /tictac [number] | tictac game \n /version  | Version \n !play <url|search> | Play a video/music  \n !skip [number] | Skip some number of songs. Will skip 1 song if a number is not specified. \n !queue | Display the current queue. \n !pause | Pause music playback. \n !resume | Resume music playback \n !leave | Clears the song queue and leaves the channel. \n !clearqueue | Clears the song queue. ```");
       console.log("Command executed: /help")
     }
     // Command /version
     if (msg.content.startsWith(prefix + "version")) {
-      msg.channel.send("``` Bot Discord Basic - Version 1.1.0 \n Créateur : IMAGOODGUY ```");
+      msg.channel.send("``` Bot Discord Basic - Version 1.2.0 \n Créateur : IMAGOODGUY ```");
       console.log("Command executed : /version")
     }
 
@@ -63,7 +68,7 @@ bot.on("message", msg => {
 
      let hit = Math.floor(Math.random() * numberHitleft) === 0
 
-      console.log(` coup restant : ${numberHitleft}, jeu : ${game}, tir : ${hit}`);
+      console.log(` coup restant : ${numberHitleft}, jeu : ${gameRussian}, tir : ${hit}`);
 
       if(numberHitleft === 6){ // check if start or is playing
         msg.channel.send("``` Chargement du révolver.. ```");
@@ -73,18 +78,72 @@ bot.on("message", msg => {
 
       if(hit){ //shot
         msg.reply(" est mort... Dommage (: ");
-        game = false;
+        gameRussian = false;
       }else{
         msg.reply(" tu n'est pas mort ! ");
         numberHitleft--;
       }
 
-      if(game === false || numberHitleft === 1){ // check if end
-        msg.channel.send("``` La partie est terminé ```");
+      if(gameRussian === false || numberHitleft === 1){ // check if end
+        msg.channel.send("``` La partie est terminée ```");
         numberHitleft = 6;
-        game = true;
+        gameRussian = true;
       }
 
+   }
+
+   if(msg.content.startsWith(prefix + "tictac")){
+     // Command /tictac
+      let number = msg.content.split(" ")[1];
+      console.log(number);
+
+      if(number > 0 && number < 10){
+        if(lastPion == "X"){ // choose wich pion has to be placed
+          lastPion = "O"
+        }else{
+          lastPion = "X"
+        }
+
+        //set the board with the player actions
+        if(gameBoard[number -1] != "X" || gameBoard[number -1] != "O"){
+          gameBoard[number -1] = lastPion;
+        }else{
+          msg.channel.send("``` Placement occupé ```");
+        }
+
+        if(gameTicTac != false && gameBoard.join(" ").indexOf("X") === -1){//check if the game has start
+          msg.channel.send("``` La partie commence ```");
+          msg.channel.send("``` " + lastPion + " a joué ```");
+          showBoard();
+        }else if(!checkBoard()){ // check if someone has won
+          msg.channel.send("``` " + lastPion + " a joué ```");
+          showBoard();
+        }else if(checkBoard()){ // reset
+          msg.channel.send("``` " + lastPion + " gagne ! ```");
+          msg.channel.send("``` La partie est terminée ```");
+          gameTicTac = true;
+          gameBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+          lastPion = "X";
+        }
+      }else{
+        msg.channel.send("``` Chiffre manquant ou érronné (1-9) ```");
+      }
+   }
+
+   function showBoard(){ // showBoard
+     msg.channel.send("``` " + gameBoard[0] + " | " + gameBoard[1] + " | " + gameBoard[2] + " \n " + gameBoard[3] + " | " + gameBoard[4] + " | " + gameBoard[5] + " \n " + gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8] + " ```");
+   }
+
+   function checkBoard(){// check all the position
+     if(gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2] || gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5] || gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8]){
+       return true;
+     }else if(gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6] || gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7] || gameBoard[3] === gameBoard[2] && gameBoard[3] === gameBoard[8]){
+       return true
+     }else if(gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8] || gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6]){
+       return true
+     }else{
+       return false
+     }
    }
 
  });
